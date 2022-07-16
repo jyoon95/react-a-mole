b5e17e3a82b40564d2c209970e6bee4234a12afd
Joseph Yoon <jyoonopop@gmail.com>
2022-07-16 13:04:39 -0400

    update

---
+++ Milestone-project-1/script.js
@@ -0,0 +1,13 @@
+//2d 2 player chess
+
+//create a 8x8 checkered board
+//create pieces (2 king, 2 queen, 4 bishop, 4 knights, 4 rooks, 16 pawns)
+//2 different colors
+
+//functionality of pieces
+//pawn move forward one space, two spaces from starting position, diagonally one space when capturing a piece
+//rook move vertical and horizontal straight line
+//knight
+//bishop move diagonal all directions
+//queen move all directions
+//king move all directions but only one space
